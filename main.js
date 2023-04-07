51dda43d72ebf7e2e4ffbb88c951d321482fb797
cham2 <dearcham2@gmail.com>
2023-04-08 03:03:38 +0900

    Start new project :D

---
+++ main.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.heigth = window.innerHeight - 100;
+
+let dino = {
+  x : 10,
+  y : 100,
+  width : 50,
+  height : 50,
+  draw(){
+    ctx.fillStyle = 'green';
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+  }
+}
+dino.draw();
+
+class Cactus{
+  constructor(){
+    this.x = 300
+    this.y = 100
+    this.width = 50
+    this.height = 50
+  }
+  draw(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+  }
+}
+
+const cactus = new Cactus
+cactus.draw()
+
