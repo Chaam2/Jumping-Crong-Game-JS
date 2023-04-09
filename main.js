cc3a25524e1e2020529f3804a71ebcc18e9573b4
cham2 <dearcham2@gmail.com>
2023-04-10 01:25:05 +0900

    Feat : 충돌체크 기능 추가

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//캔버스 설정
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth - 100;
 canvas.heigth = window.innerHeight - 100;
 
+//오브젝트 정의
 let dino = {
   x : 100,
   y : 100,
@@ -29,19 +31,32 @@ class Cactus{
   }
 }
 
-
+//애니메이션에 필요한 변수들
 let cactusArr = []
 let cactusTimer = 0
 let isJumping = false
 let jumpTimer = 0
+let startAni;
 document.addEventListener('keydown',(e)=>{
   if(e.code === "Space"){
     isJumping = true
   }
 })
 
+//충돌체크
+function collisionCheck(dino, cactus){
+  const xDiff = cactus.x - (dino.x + dino.width)
+  const yDiff = cactus.y - (dino.y + dino.height)
+  if (xDiff < 0 && yDiff < 0) { // 충돌이 발생했으면서
+    if (cactus.x + cactus.width > dino.x) { // 선인장이 디노보다 오른쪽에 있을 경우
+      cancelAnimationFrame(startAni);
+    }
+  }
+}
+
+//애니메이션 실행
 function animation(){
-  requestAnimationFrame(animation)
+  startAni = requestAnimationFrame(animation)
   ctx.clearRect(0,0,canvas.width,canvas.height)
   ctx.strokeRect(0,0,canvas.width,canvas.height)
   cactusTimer++
@@ -53,6 +68,7 @@ function animation(){
     if(cactus.x<1){
       arr.splice(index,1)
     }
+    collisionCheck(dino,cactus)
     cactus.x-=3
     cactus.draw()
   })
@@ -60,7 +76,7 @@ function animation(){
   dino.draw()
   if(isJumping){
     jumpTimer++
-    dino.y -=3
+    dino.y -=4 
   }
   if(jumpTimer>30){
     isJumping = false
